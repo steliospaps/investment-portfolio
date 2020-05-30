acc372963fbddd8b4251e5f8d9e320c9a1be878b
stelios <stelios05@gmail.com>
2020-05-30 22:24:33 +0100

    project

---
+++ investment-portfolio/src/main/java/io/github/steliospaps/experimental/investment/invest/InvestmentPortfolioApplication.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.experimental.investment.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentPortfolioApplication.class, args);
+	}
+
+}
---
+++ investment-portfolio/src/test/java/io/github/steliospaps/experimental/investment/invest/InvestmentPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.experimental.investment.invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
